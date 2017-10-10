bc46f39e144b250fff8fba05cd922832aeb59fa3
Takvir <1430275@iub.edu.bd>
2017-10-10 21:15:26 +0600

    Assignment5

--- movieQuery.js
+++ movieQuery.js
@@ -4,27 +4,40 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" },
-  { title: "Iron Man3, year: 2013", length:140,rating: "7.4",genre:"scie-fi"},
+  { title: "The Matrix Reloaded", year: "2003", length: 138, rating: "7.2", genre: "sci-fi"},
+  { title: "Avenger", year: "2008", lenght: "140", rating: "7.8", genre: "sci-fi"}
 ];
 
 function getMovieTitle( index ){
-  return movieList[index].title;
+  return movieList[3].title;
 }
 
 console.log(getMovieTitle(3));
-console.log(getMovieTitle(0));
 
 function addMovie( movie ){
   movieList.push(movie);
+
   return movieList;
 }
 
-var newMovie = {title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi"};
+var newMovie = 
+{
+  title: "Avenger",
+  year: "2008",
+  length: "140",
+  rating: "7.8",
+  genre: "sci-fi"
+};
+
 console.log(addMovie(newMovie));
 
 function movieByRating(){
-  //add code
+  movieSort = movieList;
+
+  movieSort.sort(function(a, b){return b.rating-a.rating});
+
+  return movieSort;
+
 }
 
 console.log(movieByRating());
